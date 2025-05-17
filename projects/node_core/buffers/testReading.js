6699c5b0831673abe38b50637a92a492417a6a8d
Zo3rb <ali.f90@outlook.com>
2025-05-18 00:23:02 +0300

    Removed Node-Core

--- projects/node_core/buffers/testReading.js
+++
@@ -1,7 +0,0 @@
-const fs = require("node:fs");
-
-let content = fs.readFileSync("./text.txt");
-content = content.toJSON();
-content = { ...content, plain: content.toLocaleString() };
-
-console.log(content);
